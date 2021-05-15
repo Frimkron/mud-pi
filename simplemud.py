15a9e9bbb2b777816261b30c10da24ab8714bbea
Anton <anton.larson@qurit.se>
2021-05-15 19:19:35 +0200

    Added help text for emote command

--- simplemud.py
+++ simplemud.py
@@ -133,6 +133,8 @@ while True:
                                  + "surroundings, e.g. 'look'")
             mud.send_message(id, "  go <exit>      - Moves through the exit "
                                  + "specified, e.g. 'go outside'")
+            mud.send_message(id, "  emote <message> - Express some emotion or similar "
+                                 "to the players in the room, e.g. 'emote gives you a hug'")
 
         # 'say' command
         elif command == "say":
